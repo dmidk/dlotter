00cf69b6f1bc2f8e1aae0ddb32f0998cda6e65fd
khintz <kah@dmi.dk>
2021-09-08 11:29:51 +0200

    Refactor input args

--- dlotter/__main__.py
+++ dlotter/__main__.py
@@ -21,6 +21,7 @@ from argparse import ArgumentDefaultsHelpFormatter
 from .prepare import prepare
 from .read import grib2Read
 from .plot import plot
+from .arguments import arguments
 
 
 class MyParser(argparse.ArgumentParser):
@@ -31,82 +32,9 @@ class MyParser(argparse.ArgumentParser):
 
 
 if __name__ == '__main__':
-    parent_parser = MyParser(
-        description='Plot data quick and dirty from NWP output',
-        formatter_class=ArgumentDefaultsHelpFormatter)
-    
-    subparser = parent_parser.add_subparsers(dest="cmd")
 
-    parser_prepare = subparser.add_parser('plot', help='Plot NWP output')
-
-    parser_prepare.add_argument('-p',
-                    '--parameters',
-                    metavar='PARAMETERS',
-                    type=str,
-                    help='Parameters to plot. Seperate with ":", eg: "t2m:w10m".',
-                    required=True)
-    
-    parser_prepare.add_argument('-f',
-                    '--filetype',
-                    metavar='FILETYPE',
-                    type=str,
-                    help='What filetype are we using? (Options are: grib2)',
-                    default='grib2',
-                    required=False)
-    
-    parser_prepare.add_argument('-d',
-                    '--directory',
-                    type=str,
-                    help='directory to read data from',
-                    default='.')
-    
-    parser_prepare.add_argument('--prefix',
-                    type=str,
-                    help='Set to prefix of files if any',
-                    default='',
-                    required=False)
-
-    parser_prepare.add_argument('--postfix',
-                    type=str,
-                    help='Set to postfix of files if any',
-                    default='',
-                    required=False)
-    
-    parser_prepare.add_argument('-o',
-                    '--output-dir',
-                    metavar='OUTDIR',
-                    type=str,
-                    help='Directory to place output into',
-                    default='.',
-                    required=False)
-
-    parser_prepare.add_argument('-l',
-                    '--limit-files',
-                    metavar='LIMIT',
-                    type=int,
-                    help='Only use the first LIMIT files. If set to 0, not limit is used. If Limit > 0, files will be sorted by name first',
-                    default=0,
-                    required=False)
-
-    parser_prepare.add_argument('-a',
-                    '--area',
-                    metavar='AREA',
-                    type=str,
-                    help='Over which area to plot (Options are: dk)',
-                    default="dk",
-                    required=False)
-
-    parser_prepare.add_argument('--verbose',
-                    action='store_true',
-                    help='Verbose output', 
-                    default=False)
-
-
-    if len(sys.argv)==1:
-        parent_parser.print_help()
-        sys.exit(2)
-
-    args = parent_parser.parse_args()
+    modargs = arguments()
+    args = modargs.get_args(sys.argv)
 
     if args.verbose:
         print('---- Input Arguments ----', flush=True)
