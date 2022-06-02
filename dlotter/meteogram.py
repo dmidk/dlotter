c141ca4b2624a8ef28e452dd1a9189af70dc655e
Kasper Hintz <kah@dmi.dk>
2022-06-02 11:09:51 +0200

    fix linting

--- dlotter/meteogram.py
+++ dlotter/meteogram.py
@@ -147,7 +147,8 @@ class meteogram:
             ax.spines['left'].set_visible(False)
             ax.tick_params(axis='both', which='major', pad=25)
 
-            plt.savefig('{}/epsplot_{}_{}.png'.format(args.output_dir, l, analysis.strftime('%Y%m%d%H%M')))
+            plt.savefig('{}/epsplot_{}_{}.png'.format(args.output_dir, l,
+                                                analysis.strftime('%Y%m%d%H%M')))
             l+=1
 
         return
